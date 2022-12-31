96c5a26c63d7b5c0d96c8044bd23314c08a79f0d
J <jamscc22@gmail.com>
2022-12-31 18:21:35 -0500

    product - get and delete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,45 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const inclMod = {include: [{model: Category}, {model: Tag}]};
+  const rs = (n) => {return res.status(n)};
+  const rj = (r, n) => {
+    switch (true) {
+      case (!n):
+        return res.json(r);
+      default:
+        rs(n);
+        return res.json(r);
+    }
+  };
+  try {Product.findAll(inclMod).then((d) => {return rj(d)})} catch (re) {return rj(re)}
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const {params} = req;
+  const inclMod = {include: [{model: Category}, {model: Tag}]};
+  const rs = (n) => {return res.status(n)};
+  const rj = (r, n) => {
+    switch (true) {
+      case (!n):
+        return res.json(r);
+      default:
+        rs(n);
+        return res.json(r);
+    }
+  };
+  const {id} = params;
+  try {Product.findByPk(id, inclMod).then((d) => {
+    const fd = (d != null);
+    switch (true) {
+      case (fd):
+        return rj(d);
+      default:
+        return rj('try another id', 400);
+    }})} catch (re) {return rj(re)}
 });
 
 // create new product
@@ -91,6 +124,28 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  const {params} = req;
+  const {id} = params;
+  const ptID = id;
+  const rs = (n) => {return res.status(n)};
+  const rj = (r, n) => {
+    switch (true) {
+      case (!n):
+        return res.json(r);
+      default:
+        rs(n);
+        return res.json(r);
+    }
+  };
+  const wID = {where: {id: ptID}};
+  try {Product.destroy(wID).then((d) => {
+    const ptD = (d != 0);
+    switch (true) {
+      case (ptD):
+        return rj(d);
+      default:
+        return rj('try another id', 400);
+    }})} catch (re) {return rj(re)}
 });
 
 module.exports = router;
